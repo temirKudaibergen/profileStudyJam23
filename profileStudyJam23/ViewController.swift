95d8827077f48dd6ef1f13672ba4118cd120b767
temirlan <k.temirlan.t@gmail.com>
2023-08-01 13:38:45 +0600

    Initial Commit

---
+++ profileStudyJam23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  profileStudyJam23
+//
+//  Created by Темирлан Кудайберген on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
